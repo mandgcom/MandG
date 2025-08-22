bb4264acec70b9fcc8554abbd37d7f89dffaf417
Mike Timm <mandgcom@gmail.com>
2025-08-22 23:15:14 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+document.querySelectorAll(".about-photo").forEach(img => {
+    img.addEventListener("click", function(){
+        let modal = document.getElementById("modal");
+        let modalImg = document.getElementById("modal-img");
+        modal.style.display = "flex";
+        modalImg.src = this.src;
+    });
+});
 
+document.querySelector(".close").addEventListener("click", function(){
+    document.getElementById("modal").style.display = "none";
+});
+
+// Закрытие по клику на фон
+document.getElementById("modal").addEventListener("click", function(e){
+    if(e.target === this) this.style.display = "none";
+});
